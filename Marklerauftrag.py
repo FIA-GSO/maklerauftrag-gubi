4ecf3febdbaa80c1c2b80011d502ceacad73b72b
JustinFuchs <j.fuchs.lev@gmail.com>
2024-02-15 08:27:08 +0100

    Kommentiert

--- Marklerauftrag.py
+++ Marklerauftrag.py
@@ -5,20 +5,22 @@ anzahlRaeume = 0;
 while(weitererRaum):
     anzahlRaeume += 1
     
-    laenge = int(input("Bitte geben sie die Länge des Raumes an(in Meter): "))
-    breite = int(input("Bitte geben sie die Breite des Raumes an(in Meter): "))
+    laenge = int(input("Bitte geben sie die Länge des Raumes an(in Meter): "))  #Eingabe der Raumlänge
+    breite = int(input("Bitte geben sie die Breite des Raumes an(in Meter): ")) #Eingabe der Raumbreite
 
-    flaeche = laenge * breite
-    gesamtflaeche += flaeche
-    durchschnitt = gesamtflaeche / anzahlRaeume
-    
-    print("Die Fläche des Raumes (oder des Teilaumes) beträgt " + str(flaeche) + " Quadratmeter")
-    print("Die Gesamtfläche der/des Wohnung/Hauses beträgt " + str(gesamtflaeche) + " Quadratmeter")
-    print("Der Durchschnitt der/des Wohnung/Hauses beträgt " + str(durchschnitt) + " Quadratmeter")
-    
-    weitererRaumInput = input("möchten sie einen weiteren Raum hinzu addieren?(ja/nein): ").lower()
+    bezeichnung = input("Bitte geben sie die Bezeichnung des Raumes an: ") #Eingabe der Raumbezeichnung
 
-    weitererRaum = weitererRaumInput == "ja"
+    flaeche = laenge * breite #Flächen-Berechnung
+    gesamtflaeche += flaeche  #Addieren der "neuen" Fläche auf die Gesamtfläche
+    durchschnitt = gesamtflaeche / anzahlRaeume #Berechnung des Durchschnitts
+    
+    print("Die Fläche des Raumes " + "(" + bezeichnung + ")" + " (oder des Teilaumes) beträgt " + str(flaeche) + " Quadratmeter")#Ausgabe der Fläche des Raumes bzw. des Teilraumes
+    print("Die Gesamtfläche der/des Wohnung/Hauses beträgt " + str(gesamtflaeche) + " Quadratmeter")#Ausgabe der Gesamtfläche
+    print("Der Durchschnitt der/des Wohnung/Hauses beträgt " + str(durchschnitt) + " Quadratmeter") #Ausgabe des Quadratmeter Durchschnitts
+    
+    weitererRaumInput = input("möchten sie einen weiteren Raum hinzu addieren?(ja/nein): ").lower() #Abfrage ob weiterer Raum
+
+    weitererRaum = weitererRaumInput == "ja" 
 
 print("Schönen Tag!")
 
