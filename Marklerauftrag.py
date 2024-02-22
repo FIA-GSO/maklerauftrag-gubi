ff32fd340c8f79eca139735343a05191ab5ea3a1
Philip Neuhäuser <philip.neuhaeuser@rewe-group.com>
2024-02-22 09:07:23 +0100

    New Version

--- Marklerauftrag.py
+++ Marklerauftrag.py
@@ -3,31 +3,40 @@ gesamtflaeche = 0
 anzahl_raeume = 0
 
 while weiterer_raum:
-    weiterer_raum += 1
+    anzahl_raeume += 1
 
     # Eingabe der Raumbezeichnung
     bezeichnung = input("Bitte geben sie die Bezeichnung des Raumes an: ")
 
-    # Eingabe der Raumlänge
-    laenge = int(input("Bitte geben sie die Länge des Raumes an(in Meter): "))
-    # Eingabe der Raumbreite
-    breite = int(input("Bitte geben sie die Breite des Raumes an(in Meter): "))
+    # Eingabe ob Teilraeume vorhanden sind
+    anzahl_teilraeume = int(input("aus wie vielen Teilräumen besteht der Raum: "
+                                  "(Bei keinen Teilräumen [1] eingeben) "))
 
-    # Flächenberechnung
-    flaeche = laenge * breite
+    gesamtflaeche_raum = 0
+    for anzahl_raum in range(anzahl_teilraeume):
+        # Eingabe der Raumlänge
+        laenge_teilraum = float(input("Bitte geben sie die Länge des " + str(anzahl_raum + 1) +
+                                      ". Raumes/Teilraumes an (in Meter): "))
+        # Eingabe der Raumbreite
+        breite_teilraum = float(input("Bitte geben sie die Breite des " + str(anzahl_raum + 1) +
+                                      ". Raumes/Teilraumes an (in Meter): "))
+
+        # Flächenberechnung
+        gesamtflaeche_raum += (laenge_teilraum * breite_teilraum)
+
     # Addieren der "neuen" Fläche auf die Gesamtfläche
-    gesamtflaeche += flaeche
+    gesamtflaeche += gesamtflaeche_raum
     # Berechnung des Durchschnitts
-    durchschnitt = gesamtflaeche / weiterer_raum
+    durchschnitt = gesamtflaeche / anzahl_raeume
 
     # Ausgabe der Fläche des Raumes bzw. des Teilraumes
     print("Die Fläche des Raumes " + "(" + bezeichnung + ")" +
-          " (oder des Teilaumes) beträgt " + str(flaeche) + " Quadratmeter")
+          " beträgt " + str(gesamtflaeche_raum) + " Quadratmeter")
     # Ausgabe der Gesamtfläche
     print("Die Gesamtfläche der/des Wohnung/Hauses beträgt " +
           str(gesamtflaeche) + " Quadratmeter")
     # Ausgabe des Quadratmeter Durchschnitts
-    print("Der Durchschnitt der/des Wohnung/Hauses beträgt " +
+    print("Der Durchschnitt der Raüme beträgt " +
           str(durchschnitt) + " Quadratmeter")
 
     # Abfrage ob weiterer Raum
@@ -37,5 +46,4 @@ while weiterer_raum:
     weiterer_raum = weiterer_raum_input == "ja"
 
 print("Schönen Tag!")
-
         
